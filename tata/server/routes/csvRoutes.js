1056c07551c3053539a264721a6e765b80b1d8fd
Anikesh994 <anikeshkumar994@gmail.com>
2026-07-09 13:30:45 +0530

    fetching latest dataset from db

--- tata/server/routes/csvRoutes.js
+++ tata/server/routes/csvRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const multer = require("multer");
-const { uploadCSV, getDatasets } = require("../controllers/csvController");
+const { uploadCSV, getDatasets, getLatestDataset } = require("../controllers/csvController");
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
 
 router.post("/upload", upload.single("file"), uploadCSV);
 router.get("/", getDatasets);
+router.get("/latest", getLatestDataset);
 
 module.exports = router;
